d5381ef73773444da26c05b27dcbd6ae5d2d5177
leohernandes <leohernandesvasc@gmail.com>
2024-09-17 19:51:43 -0300

    dbs: create DatabaseSetup project using api classes

---
+++ Microdados.Enem.Api/DatabaseSetup/Program.cs
@@ -0,0 +1,11 @@
+﻿using Core.DbData;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        var foo = new Item { };
+        System.Console.WriteLine(foo.ItemId);
+    }
+}
