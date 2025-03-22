2fa168cf319338721f7b21882d5a8acb301f69a1
leohernandes <leohernandesvasc@gmail.com>
2025-03-22 17:33:49 -0300

    fix(app): fix dotenv asynchronous inicialization

--- microdados_enem_app/lib/main.dart
+++ microdados_enem_app/lib/main.dart
@@ -1,11 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:microdados_enem_app/entrypoint/dotenv_initializer.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:microdados_enem_app/home/ui/home_page.dart';
 
 Future<void> main() async {
-  AppDotenv.initialize();
+  WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(MyApp());
+  try {
+    const bool isProduction = bool.fromEnvironment('dart.vm.product');
+    await dotenv.load(fileName: isProduction ? './prod.env' : './dev.env');
+  } catch (e) {
+    throw Exception('Error loading .env file: $e');
+  }
+
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
