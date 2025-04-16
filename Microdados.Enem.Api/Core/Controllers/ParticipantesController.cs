7a4dd97a88a28927fff7e1b4cee44388c8053fd4
leohernandes <leohernandesvasc@gmail.com>
2025-04-16 20:01:55 -0300

    feat(api): create get participant score

--- Microdados.Enem.Api/Core/Controllers/ParticipantesController.cs
+++ Microdados.Enem.Api/Core/Controllers/ParticipantesController.cs
@@ -10,6 +10,14 @@ namespace Core.Controllers
         private AppDbContext DbContext { get; set; } = dbContext;
 
         public record PostSubscriptionValidateRequest(string Subscription);
+        public record GetParticipantScoreResponse(
+            float ScoreCH,
+            float ScoreCN,
+            float ScoreLC,
+            float ScoreMT,
+            float ScoreRE,
+            float ScoreMean
+         );
 
         [HttpPost]
         [Route("subscription/validate")]
@@ -24,6 +32,29 @@ namespace Core.Controllers
             {
                 return BadRequest();
             }
+        }
+
+        [HttpGet]
+        [Route("participant/score")]
+        public async Task<IActionResult> GetParticipantScore([FromQuery] string subscription)
+        {
+            var scores = await DbContext.Participantes
+                .Where(p => p.ParticipanteId == subscription)
+                .Select(p => new { p.NotaCH, p.NotaCN, p.NotaLC, p.NotaMT, p.NotaRE })
+                .FirstOrDefaultAsync();
+
+            if (scores == null) return BadRequest();
+
+            float mean = (scores.NotaCH + scores.NotaCN + scores.NotaLC + scores.NotaMT + scores.NotaRE) / 5;
+
+            return Ok(new GetParticipantScoreResponse(
+                ScoreCH: scores.NotaCH,
+                ScoreCN: scores.NotaCN,
+                ScoreLC: scores.NotaLC,
+                ScoreMT: scores.NotaMT,
+                ScoreRE: scores.NotaRE,
+                ScoreMean: mean
+            ));
         }
     }
 }
